4a2226cbab0f63692d56917aa5fe6454dd3cd7e0
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-12-03 12:26:26 +0100

    Index Jumbotron

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
 <div class="jumbotron text-center">
     <h1>{{$title}}</h1>
     <p>Fell free to contribute and use this project</p>
-    <a class="btn btn-primary btn-lg" href="/signup" role="button" >Signup</a>
-    <a class="btn btn-success btn-lg" href="/login" role="button">Login</a>
+    <a class="btn btn-primary " href="/signup" role="button" >Register</a>
+    <a class="btn btn-success " href="/login" role="button">Login</a>
 </div>
 @endsection 
