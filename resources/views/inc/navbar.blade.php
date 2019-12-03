912ec28aa5bb893baf28ffa970f53478fb15cf43
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-12-03 12:09:28 +0100

    Initial commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+        <button class="navbar-toggle collapsed">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="/" class="navbar-brand">{{config('app.name', 'Laravelproject')}}</a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar">
+        <ul class="nav navbar">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/services">Services</a></li>
+        </ul>
+    </div>
+    </div>
+</nav>
