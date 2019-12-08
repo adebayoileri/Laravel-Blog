3c164806c870d760938d37383c51402e73f7a94a
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-12-08 16:38:21 +0100

    Laravelcollective/html form :)

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
     <h1>Create a Post</h1>
+        {!! Form::open(['action'=>'PostsController@store', 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title',  'Title')}}
+            {{Form::text('title','', ['class' =>'form-control', 'placeholder' => "Title"])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('body',  'Body')}}
+            {{Form::textarea('body','', ['class' =>'form-control', 'placeholder' => "Post Body"])}}
+        </div>
+    {!! Form::close() !!}
+    </div>
 @endsection
